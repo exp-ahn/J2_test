f607b9f0b78ff37d01e5a35b5886556815186d9c
exp-ahn <wltn24996@gmail.com>
2024-09-28 23:04:25 +0900

    temp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+const openNewTab = (address) => {
+    window.open(address);
+};
+
 function App() {
     return (
         <div className="App">
@@ -9,6 +13,7 @@ function App() {
                 <p>
                     test <code>src/App.js</code> test test test
                 </p>
+                <a href='https://j2-fitness.netlify.app/'>let's go netlify</a>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
